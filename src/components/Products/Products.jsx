e1048624a89b36d3dc4074637371f9451f9e61ee
Sushant <sushant.adhikari.619@gmail.com>
2021-01-28 20:35:46 +0545

    getting  products dynamically from commerce.js

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260' },
-    { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://images.pexels.com/photos/3746947/pexels-photo-3746947.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' },
-];
 
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
 
     return (
---
+++ src/lib/commerce.js
@@ -0,0 +1,5 @@
+import Commerce from '@chec/commerce.js';
+
+export const commerce = new Commerce('pk_test_225073203f9d3420d0be508a7a91820744ce3fa9b6517', true);
+
+
